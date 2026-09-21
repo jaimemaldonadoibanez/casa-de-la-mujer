0b371a777bb78fa30debc0f040fe99a0498c9437
jaimemaldonadoibanez <jaimito2162@gmail.com>
2026-09-21 12:36:11 -0400

    Ajustar basename para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./App.jsx"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
 
-    <BrowserRouter basename="/casa-de-la-mujer">
+    <BrowserRouter basename="/casa-de-la-mujer/">
       <App />
     </BrowserRouter>
 
